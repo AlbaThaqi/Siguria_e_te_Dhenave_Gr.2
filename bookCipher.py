8fb97f47e1d488e8ee5971ecb439157b65ca2025
Alba Thaqi <112955653+AlbaThaqi@users.noreply.github.com>
2023-04-08 16:38:48 +0200

    Procesi i kerkimit

--- bookCipher.py
+++ bookCipher.py
@@ -18,4 +18,19 @@ def encipher(sourcemessagefilename, bookfilename, outputfilename):
         enciphered = []
         sourceText = sf.read()
 
-        
+        for sourcechar in sourceText:
+
+            # per te evituar zgjedhjen e te njejtes pozite per secilin karakter cdo here, fillojme nga nje pozite random
+            # kerkimi behet deri ne fund dhe pastaj nese ende nuk eshte gjetur behet kerkimi nga fillimi i fajllit deri te pozita fillestare e kerkimit
+
+            startpos = random.randrange(booksize)
+
+            charpos = booktext.index(sourcechar, startpos)
+            if (charpos == -1):
+                charpos = booktext.index(sourcechar, 0, startpos)
+
+            if (charpos == -1):
+                raise ValueError("Character : " + sourcechar +
+                                 ", was not found in the book")
+
+            enciphered.append(charpos)
